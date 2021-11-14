647c9d544a87ac8af34f48217fa6111503aebb65
carlosalbertofilho <carlosalberto_filho@outlook.com>
2021-11-14 12:58:46 -0300

    Refatoração em pastas e atividades da aula 02

--- 2_Periodo/ProgramaçãoBasica/aula02/exercicio02.c
+++
@@ -1,9 +0,0 @@
-/* Exemplo de uso da biblioteca stfio.h */
-#include <stdio.h>
-int main()
-{
-  int num;
-  printf("Informe o número: ");
-  scanf("%d", &num);
-  printf("\n Você informou o número %d", num);
-} 
